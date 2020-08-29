7655c2dbe97f4c77ebea75dc1f64307cf5f3c2db
corey-marchand <corey.marchand@me.com>
2020-08-28 17:10:22 -0700

    adding default styling

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // styles
-import "assets/css/bootstrap.min.css";
-import "assets/scss/paper-kit.scss?v=1.2.0";
-import "assets/demo/demo.css?v=1.2.0";
+// import "assets/css/bootstrap.min.css";
+// import "assets/scss/paper-kit.scss?v=1.2.0";
+// import "../../assets/demo/demo.css?v=1.2.0";
 // pages
 import Index from '../src/pages/Index';
 
