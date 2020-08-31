7663396f0e0ab58454227394b337b5a752d305cf
corey-marchand <corey.marchand@me.com>
2020-08-30 19:42:14 -0700

    added icon to about me section

--- src/pages/index-sections/about.js
+++ src/pages/index-sections/about.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
-// import Icon from '../assets/developer-icon.png';
+import Icon from '../../assets/img/coder-icon.jpg';
+import Container from 'react-bootstrap/Container';
 
 const Styles = styled.div`
-h2 {
-    font-size: 300px;
-    color: #20B2AA;
-    position: fixed;
-    margin-top: 375px;
-    font-family: 'Open Sans', sans-serif;
-    margin-right: 400px;
-    margin-left: auto;
-}
+    h2 {
+        font-weight: lighter;
+        font-size: 60px;
+        text-align: center;
+    }
+
+    div.styles {
+        content-align: center;
+        width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+        border-color: #1A2126;
+        border: double #1A2126;
+    }
+
+    p {
+        text-align: center;
+    }
+
+    .img {
+        margin-left: 15%;
+        margin-right: 15%;
+        content-align: center;
+        width: 70%;
+    }
 `;
 
 
 const About = () => (
     <Styles>
-            <h2 className="mr-1 ml-5">About Me</h2>
+        <Container>
+            <h2>About Me</h2>
+            <br />
+            <div className="styles"></div>
+            <br />
+            <br />
+            <img className="img" src={Icon} alt="developer-icon" />
+            <p>I am a full-stack JavaScript/Python Software Developer located outside of the Seattle area.  Before becoming a Software Developer, I worked as a Sales Rep for a flooring company.  After working there for two years, I decided that it was not the career for myself.  The day to day task was the same thing everyday and I am somebody that needs variation.  That is when I discovered coding and fell in love with it.  I loved the challenges I faced everyday, the constant tinkering, and the challenges I constantly faced everyday.  I'm a big believer of always trying to learn new things, so naturally I fell in love with trying to master coding. Even though I am far from being a master at coding, I strive to become better with everyday that passes.</p>
+        </Container>
     </Styles>
 )
 
